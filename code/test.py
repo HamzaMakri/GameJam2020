a16d1d9ad66ae88ad1eb8cb992645e370ccdac58
HamzaMakri <47712580+HamzaMakri@users.noreply.github.com>
2020-02-03 17:28:32 +0100

    Koopa qui bouge EN DIAGONAL !!

--- code/test.py
+++ code/test.py
@@ -4,7 +4,7 @@ from pygame.locals import *
 pygame.init()
 
 # Ouverture de la fenêtre Pygame
-fenetre = pygame.display.set_mode((1280, 720), RESIZABLE)
+fenetre = pygame.display.set_mode((1024, 768), RESIZABLE)
 
 # Chargement et collage du fond
 fond = pygame.image.load("image/imageTest.jpg").convert()
@@ -17,17 +17,23 @@ koopa = pygame.image.load("image/perso.png").convert_alpha()
 position_perso = koopa.get_rect()
 fenetre.blit(koopa, position_perso)
 
-
 # Rafraîchissement de l'écran
 pygame.display.flip()
 
 continuer = 1
 
-
-pygame.key.set_repeat(1, 30)
+pygame.key.set_repeat(400, 30)
 
 # BOUCLE INFINIE
 continuer = 1
+
+up = False
+down = False
+left = False
+right = False
+
+
+
 while continuer:
     for event in pygame.event.get():  # Attente des événements
         if event.type == QUIT:
@@ -35,16 +41,42 @@ while continuer:
         if event.type == KEYDOWN:
             if event.key == K_DOWN:  # Si "flèche bas"
                 # On descend le perso
-                position_perso = position_perso.move(0, 3)
+                down = True
             if event.key == K_UP:  # Si "flèche bas"
                 # On descend le perso
-                position_perso = position_perso.move(0, -3)
+                up = True
             if event.key == K_LEFT:  # Si "flèche bas"
                 # On descend le perso
-                position_perso = position_perso.move(-3, 0)
+                left = True
             if event.key == K_RIGHT:  # Si "flèche bas"
                 # On descend le perso
-                position_perso = position_perso.move(3, 0)
+                right = True
+        elif event.type == KEYUP:
+            if event.key == K_DOWN:  # Si "flèche bas"
+                # On descend le perso
+                down = False
+            if event.key == K_UP:  # Si "flèche bas"
+                # On descend le perso
+                up = False
+            if event.key == K_LEFT:  # Si "flèche bas"
+                # On descend le perso
+                left = False
+            if event.key == K_RIGHT:  # Si "flèche bas"
+                # On descend le perso
+                right = False
+
+    if up:
+        position_perso = position_perso.move(0, -1)
+
+    if down:
+        position_perso = position_perso.move(0, 1)
+
+    if left:
+        position_perso = position_perso.move(-1, 0)
+    if right:
+        position_perso = position_perso.move(1, 0)
+
+
 
     # Re-collage
     fenetre.blit(fond, (0, 0))
@@ -55,11 +87,4 @@ while continuer:
 
 print("Hello World")
 
-
 pygame.init()
-
-
-
-
-
-
