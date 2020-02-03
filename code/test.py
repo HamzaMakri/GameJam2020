d76d05e0523247d2b3d5f87c0ff587ff9a6f25cd
HamzaMakri <47712580+HamzaMakri@users.noreply.github.com>
2020-02-03 16:23:33 +0100

    Koopa qui bouge

--- code/test.py
+++ code/test.py
@@ -3,22 +3,60 @@ from pygame.locals import *
 
 pygame.init()
 
+# Ouverture de la fenêtre Pygame
+fenetre = pygame.display.set_mode((1280, 720), RESIZABLE)
 
-fenetre = pygame.display.set_mode((1280,720), RESIZABLE)
-fond = pygame.image.load("background.jpg").convert()
-koopa = pygame.image.load("perso.png").convert()
+# Chargement et collage du fond
+fond = pygame.image.load("image/imageTest.jpg").convert()
+fenetre.blit(fond, (0, 0))
+
+# Chargement et collage du personnage
+koopa = pygame.image.load("image/perso.png").convert_alpha()
+position_perso = koopa.get_rect()
+fenetre.blit(koopa, position_perso)
 
 
-fenetre.blit(fond, (0,0))
-perso = pygame.image.load("perso.png").convert()
-fenetre.blit(perso, (200,300))
-
+# Rafraîchissement de l'écran
 pygame.display.flip()
 
-youssra=19
 continuer = 1
 
+
+pygame.key.set_repeat(1, 30)
+
+# BOUCLE INFINIE
+continuer = 1
 while continuer:
-    continuer = int(input())
+    for event in pygame.event.get():  # Attente des événements
+        if event.type == QUIT:
+            continuer = 0
+        if event.type == KEYDOWN:
+            if event.key == K_DOWN:  # Si "flèche bas"
+                # On descend le perso
+                position_perso = position_perso.move(0, 3)
+            if event.key == K_UP:  # Si "flèche bas"
+                # On descend le perso
+                position_perso = position_perso.move(0, -3)
+            if event.key == K_LEFT:  # Si "flèche bas"
+                # On descend le perso
+                position_perso = position_perso.move(-3, 0)
+            if event.key == K_RIGHT:  # Si "flèche bas"
+                # On descend le perso
+                position_perso = position_perso.move(3, 0)
+
+    # Re-collage
+    fenetre.blit(fond, (0, 0))
+    fenetre.blit(koopa, position_perso)
+    # Rafraichissement
+    pygame.display.flip()
 
 print("Hello World")
+
+
+pygame.init()
+
+
+
+
+
+
